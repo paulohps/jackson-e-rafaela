079eff8eeedb6183d86a5e8dc5d74d7a3be8eb45
Paulo Scheuermann <paetzold44@gmail.com>
2024-01-03 22:45:42 -0400

    fix assets

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/assets/css/site/index.css',
+                'resources/assets/js/site/index.js'
+            ],
+            refresh: true
         }),
     ],
 });
