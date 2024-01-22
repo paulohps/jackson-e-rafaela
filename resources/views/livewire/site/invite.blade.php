44ca40d118d94da504ed9bb833f1379f72b2572a
Paulo Scheuermann <paetzold44@gmail.com>
2024-01-21 23:14:45 -0400

    wip

--- resources/views/livewire/site/invite.blade.php
+++ resources/views/livewire/site/invite.blade.php
@@ -1,5 +1,9 @@
 <div class="flex flex-col justify-center items-center py-4 min-h-screen w-screen">
     <div class="flex flex-col space-y-8 justify-center items-center h-full w-full max-w-[95%] lg:max-w-[800px] container mx-auto text-center border-2 py-4 lg:py-8 border-dashed border-primary-900">
+        <div class="w-[96%] border-b-2 pb-4 lg:pb-8 border-primary-900 border-dashed font-bold">
+            <a href="{{ route('site.presence', $invite) }}">Clique aqui para confirmar presença</a>
+        </div>
+
         <img class="h-28 lg:h-32 inline-block" src="{{ vite_asset('images/brand-primary.png') }}" alt="Logo Jackson e Rafaela">
 
         <p class="font-montserrat font-light text-xl lg:text-2xl">COM A BENÇÃO DE SEUS PAIS</p>
@@ -14,8 +18,8 @@
         <p class="font-montserrat text-lg font-light">AVENIDA BRASIL <br> CIDADE CAMPO NOVO DO PARECIS</p>
         <img class="h-12 lg:h-20 fill-primary-900" src="{{ vite_asset('images/invite-flowers.png') }}" alt="Detalhe com flores">
 
-        <div class="w-[96%] border-t-2 pt-4 lg:pt-8 border-primary-900 border-dashed">
-            <a href="{{ route('site.presence', $invite) }}">Confirmar Presença</a>
+        <div class="w-[96%] border-t-2 pt-4 lg:pt-8 border-primary-900 border-dashed font-bold">
+            <a href="{{ route('site.presence', $invite) }}">Clique aqui para confirmar presença</a>
         </div>
     </div>
 </div>
